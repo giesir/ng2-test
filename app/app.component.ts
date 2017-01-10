c0dd6141d86197398772c57581b10a99475eb002
Giedrius Sirevicius <giedrius@icybit.co>
2017-01-10 18:43:25 +0200

    Added bindings to input

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <h1>Hello {{name}}</h1>
+    <div>
+      <label>Name:</label>
+      <input [(ngModel)]="name" placeholder="Name">
+    </div>
+  `,
 })
 export class AppComponent  { name = 'Angular'; }
